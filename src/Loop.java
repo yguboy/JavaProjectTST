c975a19695633ec487d769d402831ab2c8ada00b
yguboy <Ygorespada14@gmail.com>
2023-09-04 10:38:42 -0300

    Testes

---
+++ src/Loop.java
@@ -0,0 +1,8 @@
+public class Loop {
+    public static void main(String[] args) {
+        int limite = 10;
+        for (int i = 1; i <= limite; i++) {
+            System.out.println("Contagem: " + i);
+        }
+    }
+}
---
+++ src/Name.java
@@ -0,0 +1,8 @@
+public class Name {
+    public static void main(String[] args) {
+        int numero1 = 10;
+        int numero2 = 5;
+        int soma = numero1 + numero2;
+        System.out.println("A soma de " + numero1 + " e " + numero2 + " é " + soma);
+    }
+}
